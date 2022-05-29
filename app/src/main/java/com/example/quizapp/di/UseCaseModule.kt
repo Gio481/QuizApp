441c3ae946a09aef6ea00cf29c5a1c67602723e1
Giorgi <Gio.beridze33@gmail.com>
2022-05-29 23:37:40 +0400

    added getQuizUseCase in di module

--- app/src/main/java/com/example/quizapp/di/UseCaseModule.kt
+++ app/src/main/java/com/example/quizapp/di/UseCaseModule.kt
@@ -1,9 +1,12 @@
 package com.example.quizapp.di
 
-import com.example.quizapp.domain.usecase.GetQuizScoreUseCase
-import com.example.quizapp.domain.usecase.GetQuizScoreUseCaseImpl
+import com.example.quizapp.domain.usecase.get_quiz.GetQuizUseCase
+import com.example.quizapp.domain.usecase.get_quiz.GetQuizUseCaseImpl
+import com.example.quizapp.domain.usecase.get_score.GetQuizScoreUseCase
+import com.example.quizapp.domain.usecase.get_score.GetQuizScoreUseCaseImpl
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single<GetQuizScoreUseCase> { GetQuizScoreUseCaseImpl(get()) }
+    single<GetQuizUseCase> { GetQuizUseCaseImpl(get()) }
 }
