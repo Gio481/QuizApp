f3add58a1ca27e09b7be461aeec3bd9dec92fed6
Giorgi <Gio.beridze33@gmail.com>
2022-06-01 13:45:21 +0400

    added user use case

--- app/src/main/java/com/example/quizapp/di/UseCaseModule.kt
+++ app/src/main/java/com/example/quizapp/di/UseCaseModule.kt
@@ -4,9 +4,12 @@ import com.example.quizapp.domain.usecase.get_quiz.GetQuizUseCase
 import com.example.quizapp.domain.usecase.get_quiz.GetQuizUseCaseImpl
 import com.example.quizapp.domain.usecase.get_score.GetQuizScoreUseCase
 import com.example.quizapp.domain.usecase.get_score.GetQuizScoreUseCaseImpl
+import com.example.quizapp.domain.usecase.get_user.GetUserUseCase
+import com.example.quizapp.domain.usecase.get_user.GetUserUseCaseImpl
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single<GetQuizScoreUseCase> { GetQuizScoreUseCaseImpl(get()) }
     single<GetQuizUseCase> { GetQuizUseCaseImpl(get()) }
+    single<GetUserUseCase> { GetUserUseCaseImpl(get()) }
 }
