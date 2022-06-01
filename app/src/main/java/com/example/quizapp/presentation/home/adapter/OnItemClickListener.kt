03496c2306309f595b12a7982ca6cfc884648489
Giorgi <Gio.beridze33@gmail.com>
2022-06-01 16:34:27 +0400

    added interface to handle item click in quiz adapter

---
+++ app/src/main/java/com/example/quizapp/presentation/home/adapter/OnItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.quizapp.presentation.home.adapter
+
+import com.example.quizapp.domain.model.QuizItems
+
+interface OnItemClickListener {
+    fun onItemCLickListener(quiz:QuizItems)
+}
