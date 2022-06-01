92616621f674058c1165bdee61c4e48c36b05985
Giorgi <Gio.beridze33@gmail.com>
2022-06-01 13:45:58 +0400

    change some functions configuration in quiz score use case

--- app/src/main/java/com/example/quizapp/domain/usecase/get_score/GetQuizScoreUseCase.kt
+++ app/src/main/java/com/example/quizapp/domain/usecase/get_score/GetQuizScoreUseCase.kt
@@ -3,5 +3,4 @@ package com.example.quizapp.domain.usecase.get_score
 interface GetQuizScoreUseCase {
     suspend fun saveQuizScore(key: String, value: Int)
     suspend fun getQuizScore(key: String): Int?
-    suspend fun clearUser()
 }
